d647912f01d013b4024e0da6efdcadfd672ce670
duykhoa1232 <leduy.khoa10a@gmail.com>
2025-04-13 17:45:51 +0700

    Theme Color

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@
 import Button from '@mui/material/Button'
 import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
-
+import Typography from '@mui/material/Typography'
 
 function App() {
 
   return (
     <>
+      <Typography variant='text.secondary' component="h2">
+      h1. Heading
+      </Typography>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="contained" disabled>
